06f4fc3a105be4c473345a81b492cb33e898f4c7
Cristianf96 <cb961218@gmail.com>
2024-10-01 22:10:40 -0500

    package

--- src/tests/app/app.test.ts
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import { app } from "@app/app";
-
-describe("GET /", () => {
-  it("should return 200 OK", async () => {
-    const response = await request(app).get("/api/v1");
-    expect(response.status).toBe(200);
-  });
-});
